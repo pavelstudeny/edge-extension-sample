5b84bbc6a1cad65e88e639174dc19980bfbf1578
Pavel Studeny <studeny@avast.com>
2018-06-26 09:46:41 +0200

    localhost test

--- js/background.js
+++ js/background.js
@@ -1,3 +1,9 @@
 browser.browserAction.onClicked.addListener(function(tab) {
-            alert("Hello world");
-          });
+  fetch('http://localhost')
+    .then(resp => {
+      console.log('ok', resp.status, resp.statusText);
+      return resp.text();
+    })
+    .then(body => console.log(body))
+    .catch(err => console.log('error', err));
+});
